0d375ac8bc8abc152f05570021fd56cbeb956066
vlimant <vlimant@cern.ch>
2017-08-11 14:55:18 -0400

    tweak on mpi driver

--- filesForTitan/keep_alive.py
+++ filesForTitan/keep_alive.py
@@ -2,6 +2,60 @@ import os
 import sys
 import time
 
+n_workers = 130
+ptss = [ 10, 20, 30, 40, 50, 60, 80, 100, 120]
+stds = [
+    0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
+    0.5, 
+    1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6
+    ]
+epoch=100
+
+#ptss = map(int,sys.argv[1].split(','))
+#stds = map(float, sys.argv[2].split(','))
+#epoch = int(sys.argv[3])
+
+n_tasks = len( ptss) * len(stds)
+print (n_tasks,"tasks to run, with",n_workers,"workers")
+if n_workers > n_tasks:
+    sys.exit( 10 )
+
+walltime = 2*60
+if n_workers > 125:
+    walltime = 6*60
+
+mpi_script = 'mpi_{0:d}.pbs'.format( n_workers )
+mpi_file = open(mpi_script,'w')
+mpi_file.write("""#!/bin/bash
+#    Begin PBS directives
+#PBS -A hep107
+#PBS -N mpi_run_{1:d}
+#PBS -j oe
+####PBS -q debug
+#PBS -l walltime={0:d}:00,nodes={1:d}
+#    End PBS directives and begin shell commands
+
+cd $MEMBERWORK/hep107
+
+export HOME=/lustre/atlas/scratch/vlimant/hep107/
+export PYTHONPATH=/ccs/proj/hep107/sft/lib/python3.5/site-packages/
+
+module load python/3.5.1
+module load python_mpi4py
+
+cd /lustre/atlas/proj-shared/hep107/DLRG/filesForTitan/
+date
+aprun -n {1:d} -N 1 python mpi_span.py {2} {3} {4:d} 
+""".format(
+        walltime, 
+        n_workers,
+        ','.join( map(str,ptss) ),
+        ','.join( map(str,stds) ),
+        epoch
+        ))
+
+sys.exit(2)
+
 current_mpi_job_id = sys.argv[1] if len(sys.argv)>1 else None
 n_submissions = 10
 while n_submissions>0:
@@ -11,6 +65,8 @@ while n_submissions>0:
             time.sleep( 60 )
 
     n_submissions -= 1
+    print (time.asctime( time.localtime()))
     print ("Submit the mpi job")
     sub = os.popen('qsub mpi.pbs')
     current_mpi_job_id = int(sub.read())
+    time.sleep(60) ##it takes some time to get the job id in showq
--- filesForTitan/mpi_span.py
+++ filesForTitan/mpi_span.py
@@ -3,16 +3,26 @@ import itertools
 import random
 import glob
 import time
+import sys
 
-
+"""
+>>>>>>> tweak on mpi driver
 ptss = [ 10, 20, 30, 40, 50, 60, 80, 100, 120]
-#stds = [0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
-stds = [0.1, 0.5, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6]
+stds = [
+    0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
+    0.5, 
+    1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6
+    ]
 
 epoch=100
+"""
+ptss = map(int,sys.argv[1].split(','))
+stds = map(float, sys.argv[2].split(','))
+epoch = int(sys.argv[3])
+
+
 dir='titanRun'
 
->>>>>>> mpi drivers
 use_gpu = False
 
 combinations = list(itertools.product(ptss, stds))
@@ -62,7 +72,7 @@ if rank == 0:
                 comm.send(a_task, dest=source, tag=START)
                 print("Sending task %d to worker %d"% (a_task, source))
         elif tag == DONE:
-            result = data
+            result = int(data)
             print("Got data from worker %d" % source)
             tasks.append( result )
             executing.remove( result )
